c6380b8179f34f423fbe6fa2026bb4fe5bee3db3
Hwayeong <joid1004@naver.com>
2024-05-16 14:23:56 +0900

    react에서 응답받기(가위바위보)

--- src/main/java/com/study/Controller/Controller42.java
+++ src/main/java/com/study/Controller/Controller42.java
@@ -1,17 +1,29 @@
 package com.study.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api/main42")
 public class Controller42 {
-    @PostMapping("sub1")
-    public void sub1(@RequestBody Map<String, Object> map) {
-        System.out.println("map = " + map);
+    @GetMapping("sub1")
+    @ResponseBody
+    public String sub1() {
+        return "어떤 데이터";
+    }
+
+    @GetMapping("/sub2")
+    // return값이 view로 해석되지 않게
+    @ResponseBody
+    public String sub2() {
+        double random = Math.random();
+        String result = "";
+        if (random < 1.0 / 3) {
+            return "가위";
+        } else if (random > 1.0 / 3 && random < 2.0 / 3) {
+            return "바위";
+        } else return "보";
     }
 }
