902ddab9e92a697dc5e176f3ed6d3643a0d1098f
Hwayeong <joid1004@naver.com>
2024-05-16 11:42:31 +0900

    /api/main41/sub3 요청처리메소드 작성

--- src/main/java/com/study/Controller/Controller41.java
+++ src/main/java/com/study/Controller/Controller41.java
@@ -16,4 +16,14 @@ public class Controller41 {
         // 역직렬화(파싱, 디코딩) JSON -> Map
         System.out.println("map = " + map);
     }
+
+    @PostMapping("sub2")
+    public void sub2(@RequestBody Map<String, Object> map) {
+        System.out.println("map = " + map);
+    }
+
+    @PostMapping("sub3")
+    public void sub3(@RequestBody Map<String, Object> map) {
+        System.out.println("map = " + map);
+    }
 }
